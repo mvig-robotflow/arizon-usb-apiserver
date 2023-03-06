ac28209780e56ca535914e15057181133a4f9866
davidliyutong <davidliyutong@sjtu.edu.cn>
2023-03-06 21:59:42 +0800

    v0.4.5
    fix: fix test.restful run out of tcp ports

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = open("requirements.txt", "r").readlines() if os.path.exists("requirem
 print("#-------------------    ", str(os.listdir("./")))
 setup(
     name="arizon-usb-apiserver",
-    version="0.4.4",
+    version="0.4.5",
     author="davidliyutong",
     author_email="davidliyutong@sjtu.edu.cn",
     description="Driver for Arizona USB Pressure Sensor",
